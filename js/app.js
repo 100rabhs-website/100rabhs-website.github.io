dc3c2e5895687a8cf855ffcc92742a45b71f0aab
the100rabh <saurabh.minni@gmail.com>
2022-07-07 20:32:02 +0530

    back to static

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const testModules = require('./test-module');
+require('./main.js');
+require('../css/main.css');
+require('./plugins.js');
+// require('../css/hero-slider.css');
+// require('../css/main.css');
+// require('../css/owl-carousel.css');
+// require('../fontawesome/css/v4-shims.css');
+// require('../fontawesome/css/all.css');
+// require('../css/bootstrap.min.css');
+require('../mastodon.widget.css');
+require('../github-widget/github-widget.css');
+// require('./vendor/modernizr-2.8.3-respond-1.4.2.min.js');
+// require('./vendor/bootstrap.min.js');
+require('../github-widget/github-widget.min.js');
+require('../feed/feed_widget.js');
+require('../mastodon.widget.js');
+
+
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+// import 'owl.carousel'
--- src/js/app.js
+++
@@ -1,3 +0,0 @@
-const testModules = require('./test-module');
-require('./main.js');
-require('../css/main.css');
