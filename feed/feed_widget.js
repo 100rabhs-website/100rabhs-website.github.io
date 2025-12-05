c9865a2999cc803ea525d44c486ba8e42f2636f5
Saurabh Minni <saurabh.minni@gmail.com>
2025-12-05 16:34:41 +0530

    Updated links to apps site. Fixed blog feed

--- feed/feed_widget.js
+++ feed/feed_widget.js
@@ -1,54 +1,43 @@
-function update_feed(feed, element) {
-  console.log('updating feed ' + feed)
-  $.get(
-    feed,
-    function (feeddata) {
-      console.log('feed = ' + feeddata)
-      var data = $(feeddata).get(1)
-      feed_html = ''
-      $(data)
-        .find('entry,item')
-        .slice(0, 10)
-        .each(function () {
-          console.log(this)
-          feed_html = feed_html + get_item($(this))
-        })
+function update_feed(feed_url, element_id) {
+  console.log('updating feed from ' + feed_url);
+  $.ajax({
+    url: feed_url,
+    dataType: 'jsonp',
+    success: function (data) {
+      console.log('Received feed data:', data);
+      let feed_html = '';
+      const entries = data.feed.entry || [];
+      
+      entries.slice(0, 10).forEach(function(entry) {
+        feed_html += get_item(entry);
+      });
 
-      var html = '<ul class="feed_list">' + feed_html + '</ul>'
-
-      $('#' + element).append(html)
+      const html = '<ul class="feed_list">' + feed_html + '</ul>';
+      $('#' + element_id).html(html); // Use .html() to replace content
     },
-    'jsonp',
-  )
+    error: function(xhr, status, error) {
+      console.error("Failed to load blog feed:", status, error);
+      $('#' + element_id).html('<p>Could not load blog posts.</p>');
+    }
+  });
 }
 
-function get_item(elem) {
-  var done = false
-  link = elem.find('link');  
-  if (link.attr('href')) {
-    link_href = link.attr('href')
-  } else {
-    link_href = link.text()
-  }
-  
-  while (!done && link != null) {    
-    if ($(link).attr('rel') == 'alternate') {
-      if ($(link).attr('href')) {
-        link_href = $(link).attr('href')
-      } else {
-        link_href = $(link).text()
-      }
-      done = true
-    } else {
-        link = $(link).next();
+function get_item(entry) {
+  let post_link = '';
+  if (entry.link && Array.isArray(entry.link)) {
+    const alternate_link = entry.link.find(link => link.rel === 'alternate');
+    if (alternate_link) {
+      post_link = alternate_link.href;
     }
   }
 
+  const post_title = entry.title ? entry.title.$t : 'Untitled Post';
+
   return `
       <li class="feed_item">
-        <a class="feed_item_link" href="${link_href}" target="_blank" rel="noopener">
-          ${elem.find('title').text()}
+        <a class="feed_item_link" href="${post_link}" target="_blank" rel="noopener">
+          ${post_title}
         </a>
       </li>
-    `
+    `;
 }
